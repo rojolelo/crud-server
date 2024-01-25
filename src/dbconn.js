b812588282b31db2bd64aac4442ff8b10c1d78d1
Ronny <49622742+rojolelo@users.noreply.github.com>
2024-01-25 09:40:50 -0400

    dbconn comments

--- src/dbconn.js
+++ src/dbconn.js
@@ -8,11 +8,18 @@ const client = new MongoClient(uri);
 let conn;
 let db;
 let collection;
+
 try {
+  // Connect to DB
   conn = await client.connect();
+
+  // Connect to Tasks DB
   db = await conn.db("tasks");
+
+  // Connect to "taskscoll" collection
   collection = db.collection("taskscoll");
 } catch (e) {
   console.error(e);
 }
+
 export default collection;
